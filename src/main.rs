d81196cbc41a63516134415518c3e3f7f99b3dd0
Nancy Mazariegos <nancymazariegos03@gmail.com>
2024-09-28 19:56:24 -0600

    Changes

--- src/main.rs
+++ src/main.rs
@@ -77,11 +77,12 @@ pub fn cast_ray(
     ray_origin: &Vec3,
     ray_direction: &Vec3,
     objects: &[Object],
-    yellow_light_position: &Vec3,
+    sun_position: &Vec3,
+    sun_intensity: f32,
     depth: u32,
 ) -> Color {
     if depth > 3 {
-        return adjust_sky_color(yellow_light_position);
+        return adjust_sky_color(sun_position);
     }
 
     let mut intersect = Intersect::empty();
@@ -98,17 +99,24 @@ pub fn cast_ray(
     }
 
     if !intersect.is_intersecting {
-        return adjust_sky_color(yellow_light_position);
+        return adjust_sky_color(sun_position);
     }
 
-    let light_dir = (yellow_light_position - intersect.point).normalize();
+    let light_dir = (sun_position - intersect.point).normalize();
     let view_dir = (ray_origin - intersect.point).normalize();
     let reflect_dir = reflect(&-light_dir, &intersect.normal).normalize();
 
-    let shadow_intensity = cast_shadow(&intersect, yellow_light_position, objects);
-    let light_intensity = 1.5 * (1.0 - shadow_intensity);
+    let shadow_intensity = cast_shadow(&intersect, sun_position, objects);
 
-    let diffuse_intensity = intersect.normal.dot(&light_dir).max(0.0).min(1.0);
+
+    let sun_height = sun_position.y.max(0.0);
+    let light_intensity = if sun_height > 0.0 {
+        sun_intensity * (sun_height / 15.0) + 1.0 
+    } else {
+        0.0
+    };
+
+    let diffuse_intensity = intersect.normal.dot(&light_dir).abs().max(0.5);
     let specular_intensity = view_dir.dot(&reflect_dir).max(0.0).powf(intersect.material.specular);
 
     let diffuse_color = if let Some(texture) = &intersect.material.texture {
@@ -119,13 +127,16 @@ pub fn cast_ray(
         intersect.material.diffuse
     };
 
-    let diffuse = diffuse_color * intersect.material.albedo[0] * diffuse_intensity * light_intensity;
-    let specular = Color::new(255, 255, 255) * intersect.material.albedo[1] * specular_intensity * light_intensity;
+    let ambient_light = if sun_position.y < 0.0 { 0.3 } else { 0.2 };
 
-    diffuse + specular
+    let diffuse = diffuse_color * intersect.material.albedo[0] * diffuse_intensity * light_intensity * (1.0 - shadow_intensity);
+    let specular = Color::new(255, 255, 255) * intersect.material.albedo[1] * specular_intensity * light_intensity * (1.0 - shadow_intensity);
+    let ambient = diffuse_color * ambient_light;
+
+    diffuse + specular + ambient
 }
 
-pub fn render(framebuffer: &mut Framebuffer, objects: &[Object], camera: &Camera, yellow_light_position: &Vec3) {
+pub fn render(framebuffer: &mut Framebuffer, objects: &[Object], camera: &Camera, sun_position: &Vec3, sun_intensity: f32) {
     let width = framebuffer.width as f32;
     let height = framebuffer.height as f32;
     let aspect_ratio = width / height;
@@ -143,7 +154,7 @@ pub fn render(framebuffer: &mut Framebuffer, objects: &[Object], camera: &Camera
             let ray_direction = normalize(&Vec3::new(screen_x, screen_y, -1.0));
             let rotated_direction = camera.base_change(&ray_direction);
 
-            let pixel_color = cast_ray(&camera.eye, &rotated_direction, objects, yellow_light_position, 0);
+            let pixel_color = cast_ray(&camera.eye, &rotated_direction, objects, sun_position, sun_intensity, 0);
 
             framebuffer.set_current_color(pixel_color.to_hex());
             framebuffer.point(x, y);
@@ -175,9 +186,6 @@ fn main() {
     let water_texture = Rc::new(Texture::new("src/Water.png"));
     let hive_texture = Rc::new(Texture::new("src/Hive.png"));
     let stone_texture = Rc::new(Texture::new("src/Stone.png"));
-
-
-
 
     let grass_material = Material::new(
         Color::black(),
@@ -212,7 +220,7 @@ fn main() {
     );
 
     let pale_yellow = Material::new(
-        Color::black(), 
+        Color::black(),
         1.0,
         [0.9, 0.1, 0.0, 0.0],
         0.0,
@@ -220,7 +228,7 @@ fn main() {
     );
 
     let water_material = Material::new(
-        Color::black(), 
+        Color::black(),
         1.0,
         [0.9, 0.1, 0.0, 0.0],
         0.0,
@@ -228,7 +236,7 @@ fn main() {
     );
 
     let hive_material = Material::new(
-        Color::black(), 
+        Color::black(),
         1.0,
         [0.9, 0.1, 0.0, 0.0],
         0.0,
@@ -236,7 +244,7 @@ fn main() {
     );
 
     let stone_material = Material::new(
-        Color::black(), 
+        Color::black(),
         1.0,
         [0.9, 0.1, 0.0, 0.0],
         0.0,
@@ -244,7 +252,6 @@ fn main() {
     );
 
     let mut objects = [
-
         Object::Cube(Cube { center: Vec3::new(0.0, 10.0, 0.0), size: 1.0, material: pale_yellow.clone() }, true), //Sol
 
 
@@ -430,17 +437,17 @@ fn main() {
         Object::Cube(Cube { center: Vec3::new(-1.0, 2.0, 3.0), size: 1.0, material: dirt_material.clone() }, false), //Tierra
         Object::Cube(Cube { center: Vec3::new(-1.0, 2.0, -3.0), size: 1.0, material: dirt_material.clone() }, false), //Tierra
         Object::Cube(Cube { center: Vec3::new(-3.0, 2.0, -1.0), size: 1.0, material: dirt_material.clone() }, false), //Tierra
-        Object::Cube(Cube { center: Vec3::new(8.0, 2.0, -3.0), size: 1.0, material: dirt_material.clone() }, false), //Tierra
-        Object::Cube(Cube { center: Vec3::new(8.0, 2.0, -2.0), size: 1.0, material: dirt_material.clone() }, false), //Tierra
-        Object::Cube(Cube { center: Vec3::new(8.0, 2.0, -1.0), size: 1.0, material: dirt_material.clone() }, false), //Tierra
-        Object::Cube(Cube { center: Vec3::new(8.0, 2.0, 0.0), size: 1.0, material: dirt_material.clone() }, false), //Tierra
-        Object::Cube(Cube { center: Vec3::new(8.0, 2.0, 1.0), size: 1.0, material: dirt_material.clone() }, false), //Tierra
-        Object::Cube(Cube { center: Vec3::new(8.0, 2.0, 2.0), size: 1.0, material: dirt_material.clone() }, false), //Tierra
-        Object::Cube(Cube { center: Vec3::new(8.0, 2.0, 3.0), size: 1.0, material: dirt_material.clone() }, false), //Tierra
-        Object::Cube(Cube { center: Vec3::new(7.0, 2.0, -3.0), size: 1.0, material: dirt_material.clone() }, false), //Tierra
-        Object::Cube(Cube { center: Vec3::new(7.0, 2.0, 2.0), size: 1.0, material: dirt_material.clone() }, false), //Tierra
-        Object::Cube(Cube { center: Vec3::new(7.0, 2.0, -2.0), size: 1.0, material: dirt_material.clone() }, false), //Tierra
-        Object::Cube(Cube { center: Vec3::new(7.0, 2.0, 3.0), size: 1.0, material: dirt_material.clone() }, false), //Tierra
+        Object::Cube(Cube { center: Vec3::new(8.0, 2.0, -3.0), size: 1.0, material: grass_material.clone() }, false), //Tierra
+        Object::Cube(Cube { center: Vec3::new(8.0, 2.0, -2.0), size: 1.0, material: grass_material.clone() }, false), //Tierra
+        Object::Cube(Cube { center: Vec3::new(8.0, 2.0, -1.0), size: 1.0, material: grass_material.clone() }, false), //Tierra
+        Object::Cube(Cube { center: Vec3::new(8.0, 2.0, 0.0), size: 1.0, material: grass_material.clone() }, false), //Tierra
+        Object::Cube(Cube { center: Vec3::new(8.0, 2.0, 1.0), size: 1.0, material: grass_material.clone() }, false), //Tierra
+        Object::Cube(Cube { center: Vec3::new(8.0, 2.0, 2.0), size: 1.0, material: grass_material.clone() }, false), //Tierra
+        Object::Cube(Cube { center: Vec3::new(8.0, 2.0, 3.0), size: 1.0, material: grass_material.clone() }, false), //Tierra
+        Object::Cube(Cube { center: Vec3::new(7.0, 2.0, -3.0), size: 1.0, material: grass_material.clone() }, false), //Tierra
+        Object::Cube(Cube { center: Vec3::new(7.0, 2.0, 2.0), size: 1.0, material: grass_material.clone() }, false), //Tierra
+        Object::Cube(Cube { center: Vec3::new(7.0, 2.0, -2.0), size: 1.0, material: grass_material.clone() }, false), //Tierra
+        Object::Cube(Cube { center: Vec3::new(7.0, 2.0, 3.0), size: 1.0, material: grass_material.clone() }, false), //Tierra
         
 
         Object::Cube(Cube { center: Vec3::new(0.0, 3.0, 0.0), size: 1.0, material: trunk_material.clone() }, false), //Tronco
@@ -522,7 +529,6 @@ fn main() {
         Object::Cube(Cube { center: Vec3::new(0.0, 9.0, 1.0), size: 1.0, material: leaves_material.clone() }, false), //Hoja
         Object::Cube(Cube { center: Vec3::new(0.0, 9.0, -1.0), size: 1.0, material: leaves_material.clone() }, false), //Hoja
         Object::Cube(Cube { center: Vec3::new(0.0, 10.0, 0.0), size: 1.0, material: leaves_material.clone() }, false), //Hoja
-
     ];
 
     let mut camera = Camera::new(
@@ -534,13 +540,14 @@ fn main() {
     let mut angle: f32 = 0.0;
     let radius = 15.0;
     let rotation_speed = 0.05;
+    let sun_intensity = 2.0;
 
     while window.is_open() && !window.is_key_down(Key::Escape) {
         angle += rotation_speed;
 
-        let yellow_light_position = Vec3::new(radius * angle.cos(), radius * angle.sin(), 0.0);
+        let sun_position = Vec3::new(radius * angle.cos(), radius * angle.sin(), 0.0);
         objects[0] = Object::Cube(Cube {
-            center: yellow_light_position,
+            center: sun_position,
             size: 1.0,
             material: pale_yellow.clone(),
         }, true);
@@ -570,7 +577,7 @@ fn main() {
             camera.orbit(0.0, rotation_speed);
         }
 
-        render(&mut framebuffer, &objects, &camera, &yellow_light_position);
+        render(&mut framebuffer, &objects, &camera, &sun_position, sun_intensity);
 
         window
             .update_with_buffer(&framebuffer.buffer, framebuffer.width, framebuffer.height)
